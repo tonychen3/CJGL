954d00d5f308e8feca258397cc30a052a53f4cca
tonychen3 <46809328+tonychen3@users.noreply.github.com>
2019-11-09 20:43:37 +0800

    start dev

--- src/Starter.java
+++ src/Starter.java
@@ -1,9 +1,6 @@
-import java.util.Date;
 
 public class Starter {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        Date date = new Date();
-        System.out.println(date);
     }
 }
