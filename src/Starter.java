34a8d76692eb9460ef650a628ff266e82b6223ea
tonychen3 <46809328+tonychen3@users.noreply.github.com>
2019-11-09 20:24:04 +0800

    the starter, with date output

--- src/Starter.java
+++ src/Starter.java
@@ -1,5 +1,9 @@
+import java.util.Date;
+
 public class Starter {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        Date date = new Date();
+        System.out.println(date);
     }
 }
