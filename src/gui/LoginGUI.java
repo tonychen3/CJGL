d602d879383896ec815cb98d573c4ced423e812a
tonychen3 <46809328+tonychen3@users.noreply.github.com>
2019-11-10 16:23:56 +0800

    登录界面

---
+++ src/gui/LoginGUI.java
@@ -0,0 +1,95 @@
+/*
+ * Created by JFormDesigner on Sun Nov 10 14:57:18 GMT+08:00 2019
+ */
+
+package gui;
+
+import java.awt.*;
+import javax.swing.*;
+
+import com.intellij.uiDesigner.core.*;
+
+/**
+ * @author chen
+ */
+public class LoginGUI extends JFrame {
+    public LoginGUI() {
+        initComponents();
+    }
+
+    public static void main(String[] args) {
+        LoginGUI loginGUI=new LoginGUI();
+        loginGUI.setVisible(true);
+        loginGUI.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    private void initComponents() {
+        // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
+        panel1 = new JPanel();
+        label1 = new JLabel();
+        textField1 = new JTextField();
+        label2 = new JLabel();
+        passwordField1 = new JPasswordField();
+        button1 = new JButton();
+
+        //======== this ========
+        Container contentPane = getContentPane();
+        contentPane.setLayout(new GridLayoutManager(1, 1, new Insets(0, 0, 0, 0), -1, -1));
+
+        //======== panel1 ========
+        {
+            panel1.setLayout(new GridLayoutManager(3, 2, new Insets(15, 15, 15, 15), -1, -1));
+
+            //---- label1 ----
+            label1.setText("\u7528\u6237\u540d\uff1a");
+            panel1.add(label1, new GridConstraints(0, 0, 1, 1,
+                    GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH,
+                    GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
+                    GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
+                    null, null, null));
+            panel1.add(textField1, new GridConstraints(0, 1, 1, 1,
+                    GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH,
+                    GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
+                    GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
+                    null, new Dimension(120, 30), null));
+
+            //---- label2 ----
+            label2.setText("\u5bc6\u7801\uff1a");
+            panel1.add(label2, new GridConstraints(1, 0, 1, 1,
+                    GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH,
+                    GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
+                    GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
+                    null, null, null));
+            panel1.add(passwordField1, new GridConstraints(1, 1, 1, 1,
+                    GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH,
+                    GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
+                    GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
+                    null, new Dimension(120, 30), null));
+
+            //---- button1 ----
+            button1.setText("\u767b\u5f55");
+            panel1.add(button1, new GridConstraints(2, 0, 1, 2,
+                    GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH,
+                    GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
+                    GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
+                    null, null, null));
+        }
+        contentPane.add(panel1, new GridConstraints(0, 0, 1, 1,
+                GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_NONE,
+                GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
+                GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
+                null, null, null));
+        pack();
+        setLocationRelativeTo(getOwner());
+        // JFormDesigner - End of component initialization  //GEN-END:initComponents
+    }
+
+    // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
+    private JPanel panel1;
+    private JLabel label1;
+    private JTextField textField1;
+    private JLabel label2;
+    private JPasswordField passwordField1;
+    private JButton button1;
+    // JFormDesigner - End of variables declaration  //GEN-END:variables
+}
